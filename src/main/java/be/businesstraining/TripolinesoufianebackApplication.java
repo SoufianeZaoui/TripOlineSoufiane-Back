f258c0a496fb44377362a54c3ebc7187c34dd704
Soufiane Zaoui <za.soufiane@gmail.com>
2019-07-18 11:43:00 +0200

    Commit initial

---
+++ src/main/java/be/businesstraining/TripolinesoufianebackApplication.java
@@ -0,0 +1,13 @@
+package be.businesstraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripolinesoufianebackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TripolinesoufianebackApplication.class, args);
+    }
+
+}
